87da36b44549721b9a11881ea4c6b8bddc9fb8fa
Andrei Dzebysh <presnia@yahoo.com>
2020-08-19 15:56:13 +0300

    Tasks solutions

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 /* eslint-disable no-console */
 
-// TODO: add more examples
+/* расстояние между двумя точками */
+const x1 = a;
+const y1 = c;
+
+const x2 = b;
+const y2 = d;
+
+const newX1 = (b - a)**2;
+const newY1 = (d - c)**2;
+
+const L = Math.sqrt(newX1 + newY1);
+
+console.log(L);
+
+/* три числа, которые удовлетворяют теореме Пифагора */
+const x1 = a;
+const x2 = b;
+const x3 = c;
+
+if (a ** 2 + b ** 2 === c ** 2) {
+  console.log('true');
+} else {
+  console.log('false');
+}
+
+/* убедиться, что три числа могут являться сторонами треугольника */
+const x1 = a;
+const x2 = b;
+const x3 = c;
+
+if (c < a + b && a < b + c && b < a + c ) {
+  console.log('true');
+} else {
+  console.log('false')
+}
