ace4c099cc3422ef0e622c90a5f694c164017d67
Andrei Dzebysh <presnia@yahoo.com>
2020-08-21 18:57:50 +0300

    Solvations without index-new.js

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-/* eslint-disable no-console */
-
 /* расстояние между двумя точками */
-const x1 = a;
-const y1 = c;
+const a = 1;
+const c = 8;
 
-const x2 = b;
-const y2 = d;
+const b = 15;
+const d = 23;
 
-const newX1 = (b - a)**2;
-const newY1 = (d - c)**2;
+const newX1 = (b - a) ** 2;
+const newY1 = (d - c) ** 2;
 
-const L = Math.sqrt(newX1 + newY1);
+const isDistance = Math.sqrt(newX1 + newY1);
 
-console.log(L);
+console.log(`Расстояние между двумя данными точками будет равняться ${isDistance}`);
 
 /* три числа, которые удовлетворяют теореме Пифагора */
-const x1 = a;
-const x2 = b;
-const x3 = c;
+const e = 7;
+const f = 9;
+const g = 11;
 
-if (a**2 + b**2 === c**2) {
-  console.log('true');
-} else {
-  console.log('false');
-}
+const theorem = e ** 2 + f ** 2 === g ** 2;
+
+const rightTriangle = theorem ? "удовлетворяют" : "не удовлетворяют"
+
+console.log(`Стороны с длинами (${e}, ${f}, ${g}) ${rightTriangle} условиям теоремы Пифагора.`)
+
 
 /* убедиться, что три числа могут являться сторонами треугольника */
-const x1 = a;
-const x2 = b;
-const x3 = c;
+const h = 8;
+const k = 12;
+const l = 10;
 
-if (c < a + b && a < b + c && b < a + c ) {
-  console.log('true');
-} else {
-  console.log('false')
-}
+const check1 = h < k + l;
+const check2 = k < h + l;
+const check3 = l < k + h;
+
+const check = check1 && check2 && check3;
+
+const isTriangle = check ? "можно" : "нельзя";
+
+console.log(`На сторонах с длинами(${h}, ${k}, ${l}) ${isTriangle} построить треугольник.`)
