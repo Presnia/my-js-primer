2511f103e6f81bed739d9386612cd80228d9aab8
Andrei Dzebysh <presnia@yahoo.com>
2020-08-26 16:10:28 +0300

    to 2020.08.26

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-/* расстояние между двумя точками */
-const a = 1;
-const c = 8;
+/* eslint-disable no-console */
 
-const b = 15;
-const d = 23;
+// заполнить массив (1)
 
-const newX1 = (b - a) ** 2;
-const newY1 = (d - c) ** 2;
+const a = [];
 
-const isDistance = Math.sqrt(newX1 + newY1);
+for (let i = 1; i <= 100; i += 1) {
+  if (i % 3 === 0 || i % 5 === 0 || i % 15 === 0) {
+    a.push(i);
+  }
+}
+for (let i = 0; i < 20; i += 1) {
+  console.log(a[i]);  
+}
 
-console.log(`Расстояние между двумя данными точками будет равняться ${isDistance}`);
-
-/* три числа, которые удовлетворяют теореме Пифагора */
-const e = 7;
-const f = 9;
-const g = 11;
-
-const theorem = e ** 2 + f ** 2 === g ** 2;
-
-const rightTriangle = theorem ? "удовлетворяют" : "не удовлетворяют"
-
-console.log(`Стороны с длинами (${e}, ${f}, ${g}) ${rightTriangle} условиям теоремы Пифагора.`)
-
-
-/* убедиться, что три числа могут являться сторонами треугольника */
-const h = 8;
-const k = 12;
-const l = 10;
-
-const check1 = h < k + l;
-const check2 = k < h + l;
-const check3 = l < k + h;
-
-const check = check1 && check2 && check3;
-
-const isTriangle = check ? "можно" : "нельзя";
-
-console.log(`На сторонах с длинами(${h}, ${k}, ${l}) ${isTriangle} построить треугольник.`)
+// произведение элементов в каждой третьей позиции
